1c38ff846293bab03d2cdb8627822cebd53f2ba5
Jack Wesley Moreira <jackwesley@hotmail.com>
2022-07-13 19:35:18 -0400

    Refatorando APIs para funcionarem em conjungo

--- RetryCircuitBreakerPolly/src/Client/Services/MusicService.cs
+++ RetryCircuitBreakerPolly/src/Client/Services/MusicService.cs
@@ -13,7 +13,8 @@ namespace Client.Services
         private readonly AsyncPolicyWrap _pollyService;
         public MusicService(HttpClient httpClient, IRetryCircuitBreakerService cbService)
         {
-            httpClient.BaseAddress = new Uri("https://httpstat.us/500");
+            //httpClient.BaseAddress = new Uri("https://httpstat.us/500");
+            httpClient.BaseAddress = new Uri("https://localhost:5002/music/");
             _httpClient = httpClient;
             _pollyService = cbService.CreatePolicyManager();
 
@@ -21,7 +22,7 @@ namespace Client.Services
         public async Task<List<Music>> GetGoodSongs()
         {
             var response = await _pollyService.ExecuteAsync(
-                    async() => await _httpClient.GetAsync("")
+                    async() => await _httpClient.GetAsync("good-songs")
                 );
 
             TratarErrosResponse(response);
